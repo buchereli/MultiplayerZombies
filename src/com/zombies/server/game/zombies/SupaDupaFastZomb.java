954f9bb6c5d0a88f1e2df7933faebc7da3f2d0e7
dhomaster <derek01778@gmail.com>
2017-07-20 13:29:44 -0400

    I added a supadupafastzomb

---
+++ src/com/zombies/server/game/zombies/SupaDupaFastZomb.java
@@ -0,0 +1,18 @@
+package com.zombies.server.game.zombies;
+
+import org.jbox2d.dynamics.World;
+
+import java.awt.*;
+
+/**
+ * Created by derekho01778 on 7/20/17.
+ */
+public class SupaDupaFastZomb extends Zombie{
+
+    public SupaDupaFastZomb(Rectangle bounds, World w) {
+        super(bounds, w);
+        speed = 500;
+        bounds.width = 8;
+        bounds.height = 8;
+    }
+}
