f9b554252d95e9005cc7d745e2539a0edbc4c5a3
buchereli <buchereli@gmail.com>
2017-07-27 13:08:33 -0400

    Added enum class

---
+++ src/main/java/com/zombies/server/game/util/Enums.java
@@ -0,0 +1,10 @@
+package com.zombies.server.game.util;
+
+/**
+ * Created by buche on 7/27/2017.
+ */
+public class Enums {
+
+    public enum Direction {NORTH, EAST, SOUTH, WEST, NORTH_EAST, NORTH_WEST, SOUTH_EAST, SOUTH_WEST};
+
+}
