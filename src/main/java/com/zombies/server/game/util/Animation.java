60499eaa28df1f9546d7e5cdf49892c04a65921a
michaelmatta <mmmatta11@gmail.com>
2017-07-27 14:29:21 -0400

    Added animation class

---
+++ src/main/java/com/zombies/server/game/util/Animation.java
@@ -0,0 +1,28 @@
+package com.zombies.server.game.util;
+
+public class Animation {
+
+    private int timeBetweenFrame;
+    private int timePast;
+    private int frame;
+    private String[] imageString;
+
+    public Animation(int timeBetweenFrame, int timePast, int frame, String[] imageString) {
+        this.timeBetweenFrame = timeBetweenFrame;
+        this.timePast = timePast;
+        this.frame = frame;
+        this.imageString = imageString;
+    }
+
+    public void update(int dt) {
+        timePast += dt;
+        if(timePast > timeBetweenFrame) {
+            frame = (frame + 1) % imageString.length;
+            timePast -= timeBetweenFrame;
+        }
+    }
+    public String get() {
+        return imageString[frame];
+    }
+
+}
