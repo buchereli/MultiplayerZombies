32fb7c3266f6a58b911fcf0b0b82c25c8de3a53d
buchereli <buchereli@gmail.com>
2017-07-27 13:40:35 -0400

    Updated image managed to handle rotations

--- src/main/java/com/zombies/client/util/ImageManager.java
+++ src/main/java/com/zombies/client/util/ImageManager.java
@@ -3,6 +3,8 @@ package com.zombies.client.util;
 import com.zombies.client.screens.Client;
 
 import javax.imageio.ImageIO;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.HashMap;
@@ -12,20 +14,60 @@ import java.util.HashMap;
  */
 public class ImageManager {
 
-    private static final HashMap<String, BufferedImage> images = new HashMap<>();
+    private static final HashMap<String, ImageRotations> images = new HashMap<>();
 
     static {
         try {
-            images.put("player", ImageIO.read(Client.class.getResourceAsStream("/player.png")));
-            images.put("zombie", ImageIO.read(Client.class.getResourceAsStream("/zombie.png")));
+            images.put("player", new ImageRotations(ImageIO.read(Client.class.getResourceAsStream("/player.png")), true));
+            images.put("zombie", new ImageRotations(ImageIO.read(Client.class.getResourceAsStream("/zombie.png")), true));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-
     public static BufferedImage get(String image) {
-        return images.get(image);
+        return images.get(image).get();
+    }
+
+    public static BufferedImage get(String image, Enums.Direction direction) {
+        return images.get(image).get(direction);
+    }
+
+
+    static class ImageRotations {
+
+        private final HashMap<Enums.Direction, BufferedImage> images = new HashMap<>();
+        private BufferedImage image;
+        private boolean rotatable;
+
+        private ImageRotations(BufferedImage image, boolean rotatable) {
+            this.image = image;
+            this.rotatable = rotatable;
+
+            if (rotatable) {
+                images.put(Enums.Direction.NORTH, image);
+                images.put(Enums.Direction.EAST, rotate(image, -.5 * Math.PI));
+            }
+        }
+
+        public BufferedImage get() {
+            return image;
+        }
+
+        public BufferedImage get(Enums.Direction direction) {
+            if (rotatable)
+                return images.get(direction);
+            else
+                return image;
+        }
+
+        private BufferedImage rotate(BufferedImage bufferedImage, double radians) {
+            AffineTransform transform = new AffineTransform();
+            transform.rotate(radians, bufferedImage.getWidth() / 2, bufferedImage.getHeight() / 2);
+            AffineTransformOp op = new AffineTransformOp(transform, AffineTransformOp.TYPE_BILINEAR);
+            bufferedImage = op.filter(bufferedImage, null);
+            return bufferedImage;
+        }
     }
 
 
